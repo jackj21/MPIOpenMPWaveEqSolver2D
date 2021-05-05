388077e1430c883a20981fafb1de24b5645e72ad
Jack Jiang <jackj21@vt.edu>
2021-05-04 23:22:00 -0400

    One last push to have everything up to date

--- q3.sh
+++ q3.sh
@@ -16,14 +16,20 @@ module load gompi
 make clean
 make all
 
-mpirun -np 1  ./wave_timing 2501 17 27 1.0 250
-mpirun -np 2  ./wave_timing 5001 17 27 1.0 250
-mpirun -np 4  ./wave_timing 10001 17 27 1.0 250
-mpirun -np 8  ./wave_timing 20001 17 27 1.0 250
-mpirun -np 16  ./wave_timing 40001 17 27 1.0 250
-mpirun -np 32  ./wave_timing 80001 17 27 1.0 250
-mpirun -np 64  ./wave_timing 160001 17 27 1.0 250
-mpirun -np 128  ./wave_timing 320001 17 27 1.0 250
-mpirun -np 256  ./wave_timing 640001 17 27 1.0 250
-mpirun -np 512  ./wave_timing 1280001 17 27 1.0 250
+echo "weak wave_timing"
+echo "=================="
+echo "p n nt time" > wave_time_weak.csv
 
+mpirun -np 1  ./wave_timing 2501 17 27 1.0 250 >> wave_time_weak.csv
+mpirun -np 2  ./wave_timing 5001 17 27 1.0 250 >> wave_time_weak.csv
+mpirun -np 4  ./wave_timing 10001 17 27 1.0 250 >> wave_time_weak.csv
+mpirun -np 8  ./wave_timing 20001 17 27 1.0 250 >> wave_time_weak.csv
+mpirun -np 16  ./wave_timing 40001 17 27 1.0 250 >> wave_time_weak.csv
+mpirun -np 32  ./wave_timing 80001 17 27 1.0 250 >> wave_time_weak.csv
+mpirun -np 64  ./wave_timing 160001 17 27 1.0 250 >> wave_time_weak.csv
+mpirun -np 128  ./wave_timing 320001 17 27 1.0 250 >> wave_time_weak.csv
+mpirun -np 256  ./wave_timing 640001 17 27 1.0 250 >> wave_time_weak.csv
+mpirun -np 512  ./wave_timing 1280001 17 27 1.0 250 >> wave_time_weak.csv
+
+echo ""
+echo ""
