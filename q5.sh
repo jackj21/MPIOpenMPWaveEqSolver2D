624f735b78af354adbf342f1d6fd00592485f5df
Jack Jiang <jackj21@vt.edu>
2021-05-06 17:16:44 -0400

    Copied q5 part b data, add q5.sh

--- q5.sh
+++ q5.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-node=128
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --time=02:00:00
 #SBATCH --partition=normal_q
 #SBATCH --account=cmda3634_rjh
